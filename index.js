534640a05bf6d36fa576698f4d3a73c6100e27e8
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-06-19 15:31:49 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+// index.js
+
+function superbowlWin(record) {
+    const winRecord = record.find(game => game.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+  
+  module.exports = superbowlWin;
+  
